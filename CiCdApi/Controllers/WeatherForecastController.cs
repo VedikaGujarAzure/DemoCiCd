19875ca9f0328ac3ef959d0232e464428782805c
VedikaGujar <vgujar@getnerdio.com>
2020-05-26 11:07:16 +0530

    error created

--- CiCdApi/Controllers/WeatherForecastController.cs
+++ CiCdApi/Controllers/WeatherForecastController.cs
@@ -35,5 +35,17 @@ namespace CiCdApi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("GetAll")]
+        public IEnumerable<WeatherForecast> GetAll()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
     }
 }
